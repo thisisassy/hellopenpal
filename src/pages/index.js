1f39b8f43258190b275960d3eca3c39f32ee40f8
Haruka Ashiuchi <chopperman1224@gmail.com>
2021-11-07 21:37:22 +0900

    一覧の見た目を整理する

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,23 +3,26 @@ import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Postmark from "../ui/Postmark/postmark"
+import * as styles from "../styles/index.module.scss"
 
 const IndexPage = ({ data }) => {
   const post = data.allMarkdownRemark.edges
   return (
     <Layout>
       <Seo />
-      <section>
-        {post.map(({ node }) => (
-          <article key={node.frontmatter.slug}>
-            <Link to={`/${node.frontmatter.slug}/`}>
-              <h2>{node.frontmatter.title}</h2>
-              <p><time dateTime={node.frontmatter.date}>{node.frontmatter.date}</time></p>
-            </Link>
-          </article>
-        ))}
+      <section className={styles.section}>
+        <ul className={styles.picks}>
+          {post.map(({ node }) => (
+            <article className={styles.picksItem} key={node.frontmatter.slug}>
+              <Link to={`/${node.frontmatter.slug}/`}>
+                <h2>{node.frontmatter.title}</h2>
+                <p><time dateTime={node.frontmatter.date}>{node.frontmatter.date}</time></p>
+              </Link>
+            </article>
+          ))}
+        </ul>
       </section>
-      <Postmark />
+      <div className={styles.postmark}><Postmark /></div>
     </Layout>
   )
 }

fd9ad252eb34fe378efebad19732d9670fada178
Haruka Ashiuchi <chopperman1224@gmail.com>
2021-12-18 20:49:16 +0900

    最新記事コンポーネントをつくる

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import LatestArticles from "../components/LatestArticles"
 import * as styles from "../styles/index.module.scss"
 
 const IndexPage = ({ data, location }) => {
@@ -10,14 +11,10 @@ const IndexPage = ({ data, location }) => {
     <Layout>
       <Seo pagetitle="海外文通のはじめかた"
         pagepath={location.pathname} />
-      <div className={styles.topHeading}>
-        <div className={styles.container}>
-          <h2>Hello penpals</h2>
-          <p>This website is portal site for people who love letter &amp; postcards.</p>
-        </div>
-      </div>
       <section className={styles.section}>
-        <h3 className={styles.heading}>Penpal / 海外文通のはじめかた</h3>
+        <LatestArticles />
+      </section>
+      <section className={styles.section}>
         <ul className={styles.picks}>
           {post.map(({ node }) => (
             <Link className={styles.link} to={`/${node.frontmatter.slug}/`}>
